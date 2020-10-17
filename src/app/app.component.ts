1815687d14a79b238a00a28e22474f1c12a1b0ee
Henrique Santos <henrique.144@outlook.com>
2020-10-17 15:39:59 -0300

    Integracao com Web APIs

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,19 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+
+import { PhotoService } from './photos/photo/photo.service';
+import { Photo } from './photos/photo/photo';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
 
-  photos: Object[] = [];
+  photos: Photo[] = [];
 
-  constructor(http: HttpClient) {
-    http
-      .get<Object[]>('http://localhost:3000/flavio/photos')
+  constructor(private photoService: PhotoService) {}
+
+  ngOnInit(): void {
+    this.photoService
+      .listFromUser('flavio')
       .subscribe(photos => this.photos = photos)
   }
-
 }
