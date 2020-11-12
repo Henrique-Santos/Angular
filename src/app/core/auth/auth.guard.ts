708935dd94aeb9effb843334d98dd788eda587a2
Henrique Santos <henrique.144@outlook.com>
2020-11-12 16:48:12 -0300

    Bloqueando acesso nao autorizado

--- src/app/core/auth/auth.guard.ts
+++ src/app/core/auth/auth.guard.ts
@@ -1,27 +1,25 @@
 import { Injectable } from '@angular/core';
-import { ActivatedRouteSnapshot, Router, RouterStateSnapshot } from '@angular/router';
+import { UserService } from '../user/user.service';
+import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, Router } from '@angular/router';
 import { Observable } from 'rxjs';
 
-import { UserService } from '../user/user.service';
-
-@Injectable({
-    providedIn: 'root'
-})
-export class AuthGuard {
+@Injectable({ providedIn: 'root'})
+export class AuthGuard implements CanActivate {
 
     constructor(
         private userService: UserService,
         private router: Router
-    ) {}
+    ) { }
 
     canActivate(
-        route: ActivatedRouteSnapshot,
+        route: ActivatedRouteSnapshot, 
         state: RouterStateSnapshot
     ): boolean | Observable<boolean> | Promise<boolean> {
-        if (this.userService.isLogged()) {
-            this.router.navigate(['user', this.userService.getUserName()])
-            return false
+
+        if(!this.userService.isLogged()){
+            this.router.navigate(['']);
+            return false;
         }
-        return true
+        return true;
     }
 }
--- src/app/home/home.routing.module.ts
+++ src/app/home/home.routing.module.ts
@@ -2,9 +2,9 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router'
 
 import { HomeComponent } from './home.component';
-import { AuthGuard } from '../core/auth/auth.guard';
 import { SignInComponent } from './signin/signin.component';
 import { SignUpComponent } from './signup/signup.component';
+import { LoginGuard } from '../core/auth/login.guard';
 
 
 
@@ -12,7 +12,7 @@ const routes: Routes = [
     { 
         path: '', 
         component: HomeComponent,
-        canActivate: [AuthGuard],
+        canActivate: [LoginGuard],
         children: [
             { 
                 path: '', 
