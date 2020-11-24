f23bd323c3f99deb5fb2be9a2ebf063377a46dae
Henrique Santos <henrique.144@outlook.com>
2020-11-24 17:44:22 -0300

    O componente

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -7,6 +7,7 @@ import { HTTP_INTERCEPTORS } from '@angular/common/http';
 import { RequestInterceptor } from './auth/request.interceptor';
 import { FooterComponent } from './footer/footer.component';
 import { AlertModule } from '../shared/alert/alert.module';
+import { LoadingModule } from '../shared/components/loading/loading.module';
 
 @NgModule({
     declarations: [
@@ -16,7 +17,8 @@ import { AlertModule } from '../shared/alert/alert.module';
     imports: [
         CommonModule,
         RouterModule,
-        AlertModule
+        AlertModule,
+        LoadingModule
     ],
     exports: [
         HeaderComponent,
