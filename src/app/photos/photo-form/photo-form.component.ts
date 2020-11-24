3d4579d9eade4e89ffc469657214423392be1ad4
Henrique Santos <henrique.144@outlook.com>
2020-11-24 16:30:58 -0300

    Upload e progresso

--- src/app/photos/photo-form/photo-form.component.ts
+++ src/app/photos/photo-form/photo-form.component.ts
@@ -1,3 +1,4 @@
+import { HttpEvent, HttpEventType, HttpResponse } from '@angular/common/http';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
@@ -5,6 +6,7 @@ import { Router } from '@angular/router';
 import { AlertService } from './../../shared/alert/alert.service';
 import { PhotoService } from './../photo/photo.service';
 import { UserService } from 'src/app/core/user/user.service';
+import { finalize } from 'rxjs/operators';
 
 @Component({
   selector: 'ap-photo-form',
@@ -16,6 +18,7 @@ export class PhotoFormComponent implements OnInit {
   photoForm: FormGroup
   file: File
   preview: string
+  percentDone = 0
 
   constructor(
     private formBuilder: FormBuilder,
@@ -37,10 +40,19 @@ export class PhotoFormComponent implements OnInit {
     const description = this.photoForm.get('description').value
     const allowComments = this.photoForm.get('allowComments').value
     this.photoService.upload(description, allowComments, this.file)
+      .pipe(finalize(() => this.router.navigate(['/user', this.userService.getUserName()])))
       .subscribe(
-        () => {
-          this.alertService.success('Upload complete!', true)
-          this.router.navigate(['/user', this.userService.getUserName()])
+        (event: HttpEvent<any>) => {
+          if(event.type == HttpEventType.UploadProgress) {
+            this.percentDone = Math.round(100 * event.loaded / event.total)
+            console.log(this.percentDone)
+          } else if(event instanceof HttpResponse) {
+            this.alertService.success('Upload complete!', true)
+          }
+        },
+        err => {
+          console.log(err)
+          this.alertService.danger('Upload error', true)
         }
       )
   }
