1d03be9d259e246c1fc3c252c9fb7db3a34d7d0e
Henrique Santos <henrique.144@outlook.com>
2020-11-26 16:12:29 -0300

    Um pouco mais sobre a validacao

--- src/app/home/signup/signup.component.ts
+++ src/app/home/signup/signup.component.ts
@@ -7,6 +7,7 @@ import { lowerCaseValidator } from 'src/app/shared/validators/lower-case.validat
 import { NewUser } from './new-user';
 import { SignUpService } from './signup.service';
 import { UserNotTakenValidatorService } from './user-not-taken.validator.service';
+import { userNamePassword } from './username-password.validator';
 
 @Component({
     templateUrl: './signup.component.html',
@@ -56,6 +57,8 @@ export class SignUpComponent implements OnInit {
                     Validators.maxLength(14),
                 ]
             ]
+        }, {
+            validator: userNamePassword
         })
 
         this.platformDetectorService.isPlatformBrowser() &&
