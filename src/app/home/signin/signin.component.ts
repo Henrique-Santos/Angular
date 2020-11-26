169a43aa71db321fcb25b54df7d57cf8b4cacf2e
Henrique Santos <henrique.144@outlook.com>
2020-11-26 16:47:10 -0300

    Trabalhando com queryParams

--- src/app/home/signin/signin.component.ts
+++ src/app/home/signin/signin.component.ts
@@ -1,6 +1,6 @@
 import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { Router } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { PlatformDetectorService } from 'src/app/core/platform-detector/platform-detector.service';
 
 import { AuthService } from '../../core/auth/auth.service';
@@ -10,6 +10,7 @@ import { AuthService } from '../../core/auth/auth.service';
 })
 export class SignInComponent implements OnInit {
 
+    fromUrl: string
     loginForm: FormGroup
     @ViewChild('userNameInput') userNameInput: ElementRef<HTMLInputElement>
 
@@ -17,10 +18,14 @@ export class SignInComponent implements OnInit {
         private formBuilder: FormBuilder,
         private authService: AuthService,
         private router: Router,
-        private platformDetectorService: PlatformDetectorService
+        private platformDetectorService: PlatformDetectorService,
+        private activatedRoute: ActivatedRoute
     ) { }
 
     ngOnInit(): void {
+        this.activatedRoute
+            .queryParams
+            .subscribe(params => this.fromUrl = params.fromUrl)
         this.loginForm = this.formBuilder.group({
             userName: ['', Validators.required],
             password: ['', Validators.required]
@@ -37,7 +42,9 @@ export class SignInComponent implements OnInit {
         this.authService
             .authenticate(userName, password)
             .subscribe(
-                () => this.router.navigate(['user', userName]),
+                () => this.fromUrl
+                    ? this.router.navigateByUrl(this.fromUrl)
+                    : this.router.navigate(['user', userName]),
                 err => {
                     console.log(err)
                     this.loginForm.reset()
