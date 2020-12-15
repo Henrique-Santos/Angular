bd6d05a6f5bf45937a9dcc75dca9eb074e37c3ef
Henrique Santos <henrique.144@outlook.com>
2020-12-15 17:33:14 -0300

    Teste de Rota

--- src/app/core/header/header.component.spec.ts
+++ src/app/core/header/header.component.spec.ts
@@ -1,16 +1,18 @@
-import { AlertModule } from './../../shared/alert/alert.module';
 import { RouterTestingModule } from '@angular/router/testing';
 import { TestBed, waitForAsync } from '@angular/core/testing';
+import { Router } from '@angular/router';
+import { of } from 'rxjs';
 
+import { AlertModule } from './../../shared/alert/alert.module';
 import { HeaderComponent } from './header.component';
 import { UserService } from './../user/user.service';
 import { MenuModule } from 'src/app/shared/components/menu/menu.module';
 import { LoadingModule } from 'src/app/shared/components/loading/loading.module';
-import { of } from 'rxjs';
 
 describe('O componente Header', () => {
     let component: HeaderComponent
     let userService: UserService
+    let router: Router
 
     beforeEach(waitForAsync(() => {
         TestBed.configureTestingModule({
@@ -27,6 +29,7 @@ describe('O componente Header', () => {
 
     beforeEach(() => {
         userService = TestBed.inject(UserService)
+        router = TestBed.inject(Router)
         spyOn(userService, 'getUser').and.returnValue(
             of({
                 name: 'Henrique',
@@ -42,5 +45,13 @@ describe('O componente Header', () => {
 
     it('Deve ser instanciado', () => {
         expect(component).toBeTruthy()
+    })
+
+    it('Deve realizar o logout', () => {
+        const spy = spyOn(userService, 'logout').and.returnValue(null)
+        const navigateSpy = spyOn(router, 'navigate')
+        component.logout()
+        expect(spy).toHaveBeenCalled()
+        expect(navigateSpy).toHaveBeenCalledWith([''])
     })
 })
